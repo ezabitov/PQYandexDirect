2d81ba701e07c2fef12a790797943ba2f700a00d
ezabitov <e.zabitov@gmail.com>
2017-08-10 17:23:58 +0300

    Update PQYandexDirect.m
         Версия 1.2
         -- добавлена обработка "YESTERDAY" и "TODAY" в датах
         -- добавлено автоматическое именование отчета
         -- ClientLogin теперь необязательный

--- PQYandexDirect.m
+++ PQYandexDirect.m
@@ -1,11 +1,11 @@
 /*
      Функция, при помощи которой мы забираем данные из API Reports Яндекс.Директ
 
-     Версия 1.1
+     Версия 1.2
+     -- добавлена обработка "YESTERDAY" и "TODAY" в датах
+     -- добавлено автоматическое именование отчета
+     -- ClientLogin теперь необязательный
 
-     Токен можно получить здесь: https://oauth.yandex.ru/authorize?response_type=token&client_id=365a2d0a675c462d90ac145d4f5948cc
-     Взято здесь: https://github.com/selesnow/ryandexdirect/blob/master/R/yadirGetToken.R. Автору спасибо
-     
      Документация по API Reports: https://tech.yandex.ru/direct/doc/reports/reports-docpage/
      Список типов отчетов: https://tech.yandex.ru/direct/doc/reports/type-docpage/
      Список полей для отчета: https://tech.yandex.ru/direct/doc/reports/fields-list-docpage/
@@ -14,9 +14,26 @@
      Создатель: Эльдар Забитов (http://zabitov.ru)
 */
 
+
+
 let
-    pqyd = (Token as text, ClientLogin as text, ReportName as text, FieldNames as text, ReportType as text, DateFrom as text, DateTo as text) =>
+    pqyd = (Token as text, ClientLogin as nullable text, FieldNames as text, ReportType as text, DateFrom as text, DateTo as text) =>
 let
+
+// Проверяем на TODAY и YESTERDAY
+    DateFrom = if DateFrom = "TODAY"
+        then Date.ToText(DateTime.Date(DateTime.LocalNow()), "yyyy-MM-dd")
+    else
+        if DateFrom = "YESTERDAY"
+            then Date.ToText(Date.AddDays(DateTime.Date(DateTime.LocalNow()), -1), "yyyy-mm-dd")
+        else DateFrom,
+
+    DateTo = if DateTo = "TODAY"
+        then Date.ToText(DateTime.Date(DateTime.LocalNow()), "yyyy-MM-dd")
+    else
+        if DateTo = "YESTERDAY"
+            then Date.ToText(Date.AddDays(DateTime.Date(DateTime.LocalNow()), -1), "yyyy-mm-dd")
+        else DateTo,
 
 // Обрабатываем параметр Fieldnames, делаем список, добавляем нужные значения
 // Все это нужно чтобы можно было просто написать список полей через запятую :)
@@ -27,13 +44,14 @@ let
     transpot = Table.Transpose(delete),
     merge = Table.CombineColumns(transpot, Table.ColumnNames(transpot),Combiner.CombineTextByDelimiter("", QuoteStyle.None),"Merged"),
     fieldnamestext = merge[Merged]{0},
+    ReportName = ReportType&"-"&DateFrom&"-"&DateTo&fieldnamestext
 
 // Присваиваем полученный токен
     AuthKey = "Bearer "&Token,
     url = "https://api.direct.yandex.com/v5/reports",
 
 // Создаем тело запроса со всеми параметрами
-    body = 
+    body =
         "<ReportDefinition xmlns=""http://api.direct.yandex.com/v5/reports"">
         <SelectionCriteria>
         <DateFrom>"&DateFrom&"</DateFrom>
@@ -49,7 +67,7 @@ let
 
 // Сам запрос
 Source = Web.Contents(url,[
-           Content = Text.ToBinary(body) , 
+           Content = Text.ToBinary(body) ,
 
 // Заголовки запроса
          Headers = [#"Authorization"=AuthKey ,
@@ -57,8 +75,8 @@ Source = Web.Contents(url,[
                     #"Accept-Language"="ru",
                     #"Content-Type"="application/x-www-form-urlencoded",
                     #"returnMoneyInMicros" = "false"]
-          
-             ]   
+
+             ]
         )
 in
     Source
